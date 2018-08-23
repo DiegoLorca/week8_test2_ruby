cd8408d277dbc388caa429c845aa5dbf1b14eb19
Diego Lorca Chacon <diego.lorca.chacon@gmail.com>
2018-08-23 02:20:19 -0300

    week8 activity16 TEST 2 reload COMPLETE

--- test2.rb
+++ test2.rb
@@ -1,55 +1,16 @@
-# def name
-#   file = File.open('student.csv', 'r')
-#   data = file.readlines.map{ |ele| ele.split(', ').map(&:chomp) }
-#   file.close
-#   data.each do |ele|
-#     #print ele
-#     puts ele[0]
-#   end
-# end
-# # name
-# #############
-# def promedio_1
-#   file = File.open('student.csv', 'r')
-#   data = file.readlines.map{ |ele| ele.split(', ').map(&:chomp) }
-#   file.close
-#   data.each do |people|
-#     suma = 0
-#     people.each do |a|
-#       suma += a.to_f
-#     end
-#     promedio_1 = suma / (people.length-1)
-#     puts promedio_1
-#   end
-# end
-# promedio
-######################################
-# def notes
-  # data = file.readlines.map{ |ele| ele.split(', ').map(&:chomp) }
-  # file.close
-  # data.each do |people|
-  #   #print people
-  #
-  #   people.delete_at(0)
-  #   print people
-  # end
-# end
-# notes
-########################################
-
 def read_student
   file = File.open('student.csv', 'r')
   data = file.readlines.map{ |ele| ele.split(', ').map(&:chomp) }
   file.close
   file_saved = File.open('promedios.csv', 'w')
   data.each do |people|
-    suma = 0
+    sum = 0
     people.each do |a|
-      suma += a.to_f
+      sum += a.to_f
     end
-    promedio = suma / (people.length-1)
+    approved = sum / (people.length-1)
 
-    file_saved.puts "El alumno #{people[0]} tiene un promedio de #{promedio}"
+    file_saved.puts "El alumno #{people[0]} tiene un promedio de #{approved}"
 
   end
   file_saved.close
@@ -58,18 +19,55 @@ def read_student
 end
 
 def total_absence
+  file = File.open('student.csv', 'r')
+  data = file.readlines.map{ |ele| ele.split(', ').map(&:chomp) }
+  file.close
 
+  puts '######################Cantidad de inasistencias######################'
+  data.each do |absence|
+    name = absence[0]
+    fault = absence.count('A')
+
+    puts "#{name} tiene #{fault} inasistencias"
+  end
+  puts '################################################################################'
 end
 
 def approved_student
+  file = File.open('student.csv', 'r')
+  data = file.readlines.map{ |ele| ele.split(', ').map(&:chomp) }
+  file.close
 
+  approved = 0
+  name = 0
+  puts 'Escribir el promedio con la cual los alumnos pasaran'
+  puts '################################################################################'
+  puts ''
+  c = gets.chomp.to_f
+  puts '######################Alumnos aprobados######################'
+  data.each do |info|
+    approved = 0
+    sum = 0
+
+    info.each do |a|
+      sum += a.to_f
+    end
+
+    name = info[0]
+    approved = sum / (info.length - 1)
+
+    if c == 0
+      puts "Alumnos que aprobados con un promedio mayor o igual a 5.0 | #{name} | Promedio: #{approved}" if approved >= 5
+    elsif c <= approved
+      puts "Alumnos que aprobados  con un promedio menor o igual a #{c} | #{name} | Promedio: #{approved}"
+    end
+  end
+  puts '################################################################################'
 end
-
-puts '################################################################################'
-puts '                      Ingresa una de estas opciones'
-puts '################################################################################'
 opcion = 0
 while opcion != 4
+  puts '                      Ingresa una de estas opciones'
+  puts '################################################################################'
   puts 'Opcion 1: Generar archivo'
   puts 'Opcion 2: Cantidad de Inasistencias'
   puts 'Opcion 3: Alumnos aprobados'
